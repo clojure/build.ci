a60efa86b093e970e1580f7f895f0f02f7af0a7e
Stuart Sierra <mail@stuartsierra.com>
2011-09-02 15:50:06 -0400

    Do not call mvn in run.sh script

--- generator/src/script/run.sh
+++ generator/src/script/run.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-cd `dirname $0`/../..
+cd `dirname $0`/../../..
 
-mvn -q clean dependency:copy-dependencies
-cd ..
 rm -rf ./hudson/jobs
 java -cp "generator/src/main/clojure:generator/src/main/resources:generator/target/dependency/*" clojure.main -m clojure.build.ci.generator
