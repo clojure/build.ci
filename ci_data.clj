6a58230301248e4599aaaec6b6e4b053ad27e226
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2014-08-21 22:40:17 +0800

    Don't run Java 1.6 on core.typed

--- ci_data.clj
+++ ci_data.clj
@@ -67,7 +67,7 @@
   {:name "core.cache"         :owners ["fogus" "ambrosebs"] :min-clojure "1.3.0"}
   {:name "core.contracts"     :owners ["fogus"]}
   {:name "core.rrb-vector"    :owners ["michalmarczyk"] :min-clojure "1.5.1" :exclude-jdk #{"Sun JDK 1.5" "IBM JDK 1.5"}}
-  {:name "core.typed"         :owners ["ambrosebs"] :min-clojure "1.5.1" :jdk-version "1.7"}
+  {:name "core.typed"         :owners ["ambrosebs"] :min-clojure "1.6.0" :exclude-jdk #{"Sun JDK 1.6" "IBM JDK 1.6" "OpenJDK 1.6"}}
   {:name "data.avl"           :owners ["michalmarczyk"] :min-clojure "1.5.1"}
   {:name "data.codec"         :owners ["ataggart"] :min-clojure "1.3.0"}
   {:name "data.csv"           :owners ["jonasenlund"]}
