1e1b49174472ea1124fd7cd759afc96f6fcbec38
Alex Miller <alex@puredanger.com>
2015-11-06 11:26:12 -0600

    Change min clojure to 1.3.0 for core.memoize

--- ci_data.clj
+++ ci_data.clj
@@ -51,7 +51,7 @@
   {:name "core.logic"         :owners ["davidnolen"] :min-clojure "1.3.0"}
   {:name "core.match"         :owners ["davidnolen"] :min-clojure "1.4.0"}
   {:name "core.unify"         :owners ["fogus"]}
-  {:name "core.memoize"       :owners ["fogus"] :min-clojure "1.2.1"}
+  {:name "core.memoize"       :owners ["fogus"] :min-clojure "1.3.0"}
   {:name "core.cache"         :owners ["fogus" "ambrosebs"] :min-clojure "1.3.0"}
   {:name "core.contracts"     :owners ["fogus"]}
   {:name "core.rrb-vector"    :owners ["michalmarczyk"] :min-clojure "1.5.1"}
