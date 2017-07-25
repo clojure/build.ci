75c9eac1deeb5f1a5893b0d33f1ed3228f29c38f
Alex Miller <alex@puredanger.com>
2017-07-25 09:25:23 -0500

    Add tools.deps.alpha

--- ci_data.clj
+++ ci_data.clj
@@ -6,7 +6,7 @@
  ;; The versions of Clojure against which we will test
  ;; contrib libraries
  :clojure-versions
- ["1.2.0" "1.2.1" "1.3.0" "1.4.0" "1.5.1" "1.6.0" "1.7.0" "1.8.0" "1.9.0-alpha16" "1.9.0-master-SNAPSHOT"]
+ ["1.2.0" "1.2.1" "1.3.0" "1.4.0" "1.5.1" "1.6.0" "1.7.0" "1.8.0" "1.9.0-alpha17" "1.9.0-master-SNAPSHOT"]
 
  ;; Installed Java versions. If :enabled is true we will test contrib
  ;; libraries with that Java version.
@@ -83,6 +83,7 @@
   {:name "tools.analyzer.jvm" :owners ["nicolamometto"] :min-clojure "1.5.1"}
   {:name "tools.emitter.jvm"  :owners ["nicolamometto"] :min-clojure "1.5.1"}
   {:name "tools.cli"          :owners ["garethjones" "sungpae" "seancorfield"]}
+  {:name "tools.deps.alpha"   :owners ["puredanger"] :min-clojure "1.8.0"}
   {:name "tools.logging"      :owners ["ataggart"] :min-clojure "1.3.0"}
   {:name "tools.macro"        :owners ["konradhinsen"]}
   {:name "tools.namespace"    :owners ["stuartsierra"] :min-clojure "1.7.0"}
