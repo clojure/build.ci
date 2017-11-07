1d48ab0032e77c2c5963da519b7aedf918ef15b1
Alex Miller <alex@puredanger.com>
2017-11-07 09:42:04 -0600

    Change min-clojure to 1.7 for data.codec

--- ci_data.clj
+++ ci_data.clj
@@ -63,7 +63,7 @@
   {:name "core.typed"         :owners ["ambrosebs"] :min-clojure "1.6.0" :exclude-jdk #{"Sun JDK 1.6" "IBM JDK 1.6" "OpenJDK 1.6"}
                               :jdk-version "1.7"}
   {:name "data.avl"           :owners ["michalmarczyk"] :min-clojure "1.5.1"}
-  {:name "data.codec"         :owners ["ataggart"] :min-clojure "1.3.0"}
+  {:name "data.codec"         :owners ["ataggart"] :min-clojure "1.7.0"}
   {:name "data.csv"           :owners ["jonasenlund"]}
   {:name "data.finger-tree"   :owners ["Chouser"] :min-clojure "1.4.0"}
   {:name "data.fressian"      :owners ["stuart.halloway"] :min-clojure "1.5.1"}
