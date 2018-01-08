0b8b64d3a16770bc4bd520aded95ad6c067106ef
Alex Miller <alex@puredanger.com>
2018-01-08 09:28:50 -0600

    Make Java 7 min jdk for core.async

--- ci_data.clj
+++ ci_data.clj
@@ -50,7 +50,8 @@
  :contribs
  [{:name "algo.monads"        :owners ["konradhinsen"]}
   {:name "algo.generic"       :owners ["konradhinsen"] :min-clojure "1.3.0"}
-  {:name "core.async"         :owners ["alexmiller","stuart.halloway"] :min-clojure "1.7.0"}
+  {:name "core.async"         :owners ["alexmiller","stuart.halloway"] :min-clojure "1.7.0" :exclude-jdk #{"Sun JDK 1.6" "IBM JDK 1.6" "OpenJDK 1.6"}
+                              :jdk-version "1.7"}
   {:name "core.incubator"     :owners []}
   {:name "core.logic"         :owners ["davidnolen"] :min-clojure "1.3.0"}
   {:name "core.match"         :owners ["davidnolen"] :min-clojure "1.4.0"}
