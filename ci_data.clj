511ad79348cd568b85d65acc9a0207ed33fd471c
Alex Miller <alex.miller@cognitect.com>
2018-10-15 16:29:51 -0500

    Deprecate core.incubator

--- ci_data.clj
+++ ci_data.clj
@@ -75,7 +75,6 @@
  [{:name "algo.monads"        :owners ["konradhinsen"]}
   {:name "algo.generic"       :owners ["konradhinsen"] :min-clojure "1.3.0"}
   {:name "core.async"         :owners ["alexmiller","stuart.halloway"] :min-clojure "1.7.0"}
-  {:name "core.incubator"     :owners []}
   {:name "core.logic"         :owners ["davidnolen"] :min-clojure "1.3.0"}
   {:name "core.match"         :owners ["davidnolen"] :min-clojure "1.4.0"}
   {:name "core.unify"         :owners ["fogus"]}
